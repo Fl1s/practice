f6b0b28feb87656834530c3246b4f4f0e4b54a11
Fl1s <maksima7th@gmail.com>
2026-06-04 10:17:53 +0700

    feat: unit tests for calculations module

--- calculations/build.gradle.kts
+++ calculations/build.gradle.kts
@@ -30,6 +30,18 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation(kotlin("test"))
+
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:core:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+    androidTestImplementation("io.mockk:mockk-android:1.13.11")
+    androidTestImplementation(kotlin("test"))
+
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
     kapt("androidx.room:room-compiler:2.8.4")
